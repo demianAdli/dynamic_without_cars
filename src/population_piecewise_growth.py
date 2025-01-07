0b4b3c5e56469f0062de967fbf06ee158f05c8da
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-01-07 14:16:00 -0500

    Add the scenario

--- src/population_piecewise_growth.py
+++ src/population_piecewise_growth.py
@@ -2,9 +2,10 @@ from BPTK_Py import sd_functions as sd
 
 from src.population_base import PopulationBase
 from src.population_exponential_growth import PopulationExponentialGrowth
+from src.population_logistic_growth import PopulationLogisticGrowth
 
 
-class PopulationPairwiseGrowth(PopulationBase):
+class PopulationPiecewiseGrowth(PopulationBase):
   def __init__(self):
     super().__init__()
 
@@ -16,5 +17,5 @@ class PopulationPairwiseGrowth(PopulationBase):
     self.population.equation = \
         (sd.If
          (sd.time() >= self.tipping_point,
-          PopulationExponentialGrowth().population,
-          PopulationExponentialGrowth().population))
+          PopulationExponentialGrowth().population.equation,
+          PopulationLogisticGrowth().population.equation))
