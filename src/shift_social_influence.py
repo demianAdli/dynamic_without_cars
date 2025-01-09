cd6ed4026177454965b3bf20ecb4cd30b2a12022
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-01-09 15:03:11 -0500

    Add the default setting for all private shift to sustainable modes classes

---
+++ src/shift_social_influence.py
@@ -0,0 +1,22 @@
+from src.shift_base import ShiftBase
+
+
+class ShiftSocialInfluence(ShiftBase):
+  def __init__(self):
+    super().__init__()
+
+    self.initialize()
+
+  def initialize(self):
+    super().initialize()
+
+    self.shift_to_sustainable_modes.equation = \
+        ((min(1, self.current_infrastructure_capacity / 100) *
+          (self.active_transportation_trip_share +
+           self.public_transport_trip_share +
+           self.ride_sharing_trip_share) /
+          max(1,
+              (self.active_transportation_trip_share +
+               self.public_transport_trip_share +
+               self.ride_sharing_trip_share))) / 100) * \
+        self.private_cars_num
