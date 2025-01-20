dbac7d3424211f7f8055351f80676bc629fc7c37
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-01-20 11:50:56 -0500

    Move average population to the pop base: it is not used in the parent

--- src/population_base.py
+++ src/population_base.py
@@ -4,8 +4,10 @@ from src.less_cars_base_dynamics import LessCarsBaseDynamics
 class PopulationBase(LessCarsBaseDynamics):
   def __init__(self):
     super().__init__(starttime=0.0, stoptime=25.0, dt=1.0, name='base_model')
+    self.average_population = self.constant('Average Population')
 
     self.initialize()
 
   def initialize(self):
+    self.average_population = 17100
     self.population.equation = None
