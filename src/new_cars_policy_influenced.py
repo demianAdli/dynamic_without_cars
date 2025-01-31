7c3b1513f315753ef1aa938acda22bbb62d3607d
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-01-31 11:21:27 -0500

    Edit the comment

--- src/new_cars_policy_influenced.py
+++ src/new_cars_policy_influenced.py
@@ -12,9 +12,9 @@ class NewCarsPolicyInfluenced(NewCarsBase):
   def initialize(self):
     super().initialize()
 
-    # minimal impact = 0.01 - 0.05
-    # moderate impact = 0.05 - 0.1
-    # high impact = 0.1 - 0.3
+    # minimal impact = 0.01 to 0.05
+    # moderate impact = 0.05 to 0.1
+    # high impact = 0.1 to 0.3
     self.policy_impact_factor.equation = 0.01
     self.new_cars_num.equation = \
         self.initial_new_cars_num * \
